c860984b9a659e08c8f5bc14880da73498cbb5d8
Danilo de Souza <dannilosn@gmail>
2025-04-28 15:20:04 -0300

    Fix: Fix call to s3 and adds logs

--- rclone-cron.sh
+++ rclone-cron.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
-echo "[INFO] $(date) - Starting sync to S3"
+BACKUP_DIR="/backups"
 
-rclone sync /backups "s3:${AWS_BUCKET}/${AWS_BUCKET_PATH}" \
+LOG_FOLDER="/logs"
+mkdir -p "$LOG_FOLDER"
+
+LOG_FOLDER="$LOG_FOLDER/rclone-backup_$(date +%Y-%m-%d-%H-%M-%S).log"
+
+echo "[INFO] $(date) - Starting sync from $BACKUP_DIR to S3" >> "$LOG_FOLDER"
+
+rclone sync $BACKUP_DIR \
+  "s3:$AWS_S3_BUCKET/$AWS_S3_BUCKET_PATH" \
   --s3-provider AWS \
+  --s3-env-auth \
   --s3-region "${AWS_REGION}" \
+  --s3-no-check-bucket \
+  --s3-force-path-style \
+  --s3-bucket-acl private \
   --progress \
-  --verbose
+  --verbose >> "$LOG_FOLDER" 2>&1
 
-echo "[INFO] $(date) - Sync completed"
+echo "[INFO] $(date) - Sync completed" >> "$LOG_FOLDER"
 
